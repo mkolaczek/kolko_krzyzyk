935d5eafc1527a3d370eacf9a81e4aae931a0649
Marcin <marcin.kolaczek@yahoo.pl>
2019-01-28 17:07:52 +0100

    2

--- src/GameController.java
+++ src/GameController.java
@@ -1,19 +1,16 @@
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseDragEvent;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.input.MouseEvent;
 
 public class GameController {
 
-//    @FXML
-//    private BorderPane BorderPane00;
-//
-//    @FXML
-//    void initialize(){
-//
-//    }
-//
-//    @FXML
-//    void touch00(MouseDragEvent event) {
-//
-//    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+    @FXML
+    void touch00(MouseEvent event) {
+        System.out.println("fdafsd");
+    }
 }
